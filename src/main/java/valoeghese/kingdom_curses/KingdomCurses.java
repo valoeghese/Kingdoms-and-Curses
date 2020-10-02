8cdc9773e3f8e6d8f41f635ceaa7a288ec00ba5e
valoeghese <valoeghesemods@gmail.com>
2020-10-02 21:29:04 +1300

    e

---
+++ src/main/java/valoeghese/kingdom_curses/KingdomCurses.java
@@ -0,0 +1,17 @@
+package valoeghese.kingdom_curses;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import java.util.Random;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KingdomCurses implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("Kingdom Curses");
+
+	@Override
+	public void onInitialize() {
+		// Add init code here!
+	}
+}
